6e93c8a3cf9989a39fa2832289aeb233e24fd3ed
oswealth <osbertjaywealth@gmail.com>
2023-10-18 22:46:41 -0700

    string

---
+++ string.c
@@ -0,0 +1,116 @@
+#include "shell.h"
+
+/**
+ * _strdup - duplicates a string
+ * @str: the string to duplicate
+ *
+ * Return: pointer to the duplicated string
+ */
+
+char *_strdup(const char *str)
+{
+	char *ptr;
+	int i, len = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+	str = str - len;
+	ptr = malloc(sizeof(char) * (len + 1));
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i <= len; i++)
+		ptr[i] = str[i];
+	return (ptr);
+}
+
+/**
+ * _strcmp - compare string values
+ * @s1: input value
+ * @s2: input value
+ *
+ * Return: s1[i] - s2[i]
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int cmp;
+
+	cmp = (int)*s1 - (int)*s2;
+	while (*s1)
+	{
+		if (*s1 != *s2)
+			break;
+		s1++;
+		s2++;
+		cmp = (int)*s1 - (int)*s2;
+	}
+	return (cmp);
+}
+
+/**
+ * _strlen - returns and checks the len of str
+ * @s: str to be checked
+ * Return: length of the str
+ *
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len])
+		len++;
+	return (len);
+}
+
+/**
+ * _strcat - concats two str
+ * @dest: destination buff
+ * @src: sourc buf
+ *
+ * Return: pointe to dest buf
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *p = dest;
+
+	while (*p)
+		p++;
+
+	while (*src)
+	{
+		*p = *src;
+		p++;
+		src++;
+	}
+	*p = *src;
+	return (dest);
+}
+
+
+/**
+ * char *_strcpy - a function that copies the str pointed to
+ * @dest: copy to
+ * @src: copy from
+ *
+ * Return: str
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
